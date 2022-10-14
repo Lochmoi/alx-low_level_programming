07a9585d5d48eaa7eb9ac456757cedd07835aaed
Lochmoi <Lochmoimicah@gmail.com>
2022-10-14 12:02:01 -0700

    Hello_puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
    * main - entry point
     *
      * Return: Always 0
- */
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
