44a09caa361570d4b5babbc114e17d4e6759815b
Lochmoi <Lochmoimicah@gmail.com>
2022-10-19 08:26:36 -0700

    Print alphabets 10x on new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "math.h"
+/**
+ * print_alphabet_x10 in lowercase - a function that prints 10 times alphabet,
+ *in a new line on lowercase
+ *return nothin
+*/
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alphabet;
+	while (count++ <= 9)
+
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			_putchar(alphabet);
+	_putchar('\n');
+	}
+}
