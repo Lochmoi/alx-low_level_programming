e72944a99269b3b0fb99d03c55873c06e424bf41
Lochmoi <Lochmoimicah@gmail.com>
2022-10-19 09:16:24 -0700

    alphabet in lowercase printed 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,10 @@ void print_alphabet_x10(void)
 	char alphabet;
 	while (count++ <= 9)
 
+
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 			_putchar(alphabet);
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
