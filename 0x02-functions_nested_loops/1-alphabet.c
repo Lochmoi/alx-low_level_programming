8acdd4667b62f4be6fb4191e952e9cd60c043946
Lochmoi <Lochmoimicah@gmail.com>
2022-10-18 18:07:59 -0700

    alphabet commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  a function that prints the alphabet, in lowercase
+   * followed by a new line
+    * Return:nothing
+*/
+void print_alphabet(void)
+{
+	int namu;
+
+	for (namu = 'a'; namu <= 'z'; namu++)
+	_putchar(namu);
+	_putchar('\n');
+}
