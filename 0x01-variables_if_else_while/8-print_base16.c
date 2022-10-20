95c605dad9302ed012e32d6effed35634cef838d
Lochmoi <Lochmoimicah@gmail.com>
2022-10-19 19:03:19 -0700

    print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main(void)
 	int a;
 	char lettr;
 
-	for (a= '0'; a<= '9'; a++)
-	putchar( a);
-	for (lettr= 'a'; lettr <= 'f'; lettr++)
+	for (a = '0'; a <= '9'; a++)
+	putchar(a);
+	for (lettr = 'a'; lettr <= 'f'; lettr++)
 	putchar(lettr);
 	putchar('\n');
 	return (0);
