ae2190b4f1a78597390915f09f1180bcc4ff736c
Lochmoi <Lochmoimicah@gmail.com>
2022-10-19 18:49:59 -0700

    print base-16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0
+*/
+int main(void)
+{
+	int a;
+	char lettr;
+
+	for (a= '0'; a<= '9'; a++)
+	putchar( a);
+	for (lettr= 'a'; lettr <= 'f'; lettr++)
+	putchar(lettr);
+	putchar('\n');
+	return (0);
+}
