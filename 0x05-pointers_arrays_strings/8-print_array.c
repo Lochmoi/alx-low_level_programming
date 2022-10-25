565c7abfa94d776c905182991499bfd308028e15
Lochmoi <Lochmoimicah@gmail.com>
2022-10-25 05:35:53 -0700

    print array commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* print_array - unction that prints n elements of an array of integer
+* @a: pointer to first int
+* @n: pointer to second int
+* Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
+	}
+	printf("\n");
+}
